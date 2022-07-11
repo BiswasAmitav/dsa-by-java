bd2569d405e27358f04f5334d9c5bda27c2fd9b1
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-11 21:24:00 +0530

    soldi pattern is udated

--- dataStructureAndAlgorithm/src/dsaWithJava/SolidPattern.java
+++ dataStructureAndAlgorithm/src/dsaWithJava/SolidPattern.java
@@ -1,6 +1,6 @@
-package javaDataStructureAndAlgorithm;
+package dsaWithJava;
 
-public class PatternSolidRectangle {
+public class SolidPattern {
     public static void main(String[] args) {
         for(int i=1;i<=4;i++){
             for(int j=1;j<=15;j++){
